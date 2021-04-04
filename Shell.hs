7cd26eb30a6565901c8b8a679039dbbed9038997
moona <shoukaku00@gmail.com>
2021-04-04 18:04:32 +0200

    a bit of work on shell.hs

--- Shell.hs
+++ Shell.hs
@@ -3,3 +3,21 @@ import System.IO
 -- First read the user name
 -- Then, write own implemetation of words, using : as separator
 -- to properly read shell name from /etc/passwd
+myWords "" = []
+myWords sentence
+        | sentence == [] = [[]]
+        | otherwise = (takeWhile (/=':') sentence) : myWords(dropWhile (==':') (dropWhile (/=':') sentence))
+readPasswd = readFile "/etc/passwd" >>= (\x -> putStrLn x)
+
+
+-- Those parts will need to be worked on more later.
+--
+--myWords' "" = []
+--myWords' sentence
+--        | sentence == [] = [[]]
+--        | otherwise = (takeWhile (/='/') sentence) : myWords'(dropWhile (=='/') (dropWhile (/='/') sentence))
+--
+--findHome :: [String] -> String
+--findHome (x:xs)
+--        | dropWhile (/= '/') x == "/home" = "home"
+--        | otherwise = findHome xs
