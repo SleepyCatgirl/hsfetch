b1a0d67853608d7d0b50b7eb4e4de45cc583d069
moona <shoukaku00@gmail.com>
2021-04-10 18:09:24 +0200

    a bit of haskell

--- CPU.hs
+++ CPU.hs
@@ -6,6 +6,12 @@ processorFunc = do
   handle <- openFile "/proc/cpuinfo" ReadMode
   contents <- hGetContents handle
   putStr (dropWhile (/=':') ((lines $ contents) !! 4) ++ "\n")
+ -- putStr "Core 0"
+ -- putStr (dropWhile (/=':') ((lines $ contents) !! 7) ++ "MHz" ++ "\n")
+ -- putStr "Core 1"
+ -- putStr (dropWhile (/=':') ((lines $ contents) !! 35) ++ "MHz" ++ "\n")
   hClose handle
 --Version while using (>>=) operator
 -- processorFunc = openFile "/proc/cpuinfo" ReadMode >>= (\x -> hGetContents x) >>= (\x -> putStr (dropWhile (/=':') ((lines $ x) !! 4) ++ "\n"))
+-- Version using Functors
+--processorFunc = putStr <$> readFile "/proc/cpuinfo"
